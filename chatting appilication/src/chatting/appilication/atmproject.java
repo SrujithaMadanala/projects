15749e6e40aff1375f9077b90b19650937760f25
SrujithaMadanala <165554185+SrujithaMadanala@users.noreply.github.com>
2025-04-16 19:33:23 +0530

    Add files via upload

---
+++ chatting appilication/src/chatting/appilication/atmproject.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chatting.appilication;
+
+/**
+ *
+ * @author M srujitha
+ */
+public class atmproject {
+    
+}
